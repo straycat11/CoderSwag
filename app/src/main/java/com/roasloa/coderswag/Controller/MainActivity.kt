e3b0fde6f2a096bf5255c5f1da9b181c8589e8cb
utku <utkuuyucel@gmail.com>
2019-01-22 13:33:22 +0300

    lesson 01

--- app/src/main/java/com/roasloa/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/roasloa/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.roasloa.coderswag
+package com.roasloa.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.roasloa.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/roasloa/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.roasloa.coderswag.Model
+
+class Category(val title: String, val image: String)
