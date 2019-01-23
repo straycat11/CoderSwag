db810f13b3d59a0173dab354055e1e31efe6c116
utku <utkuuyucel@gmail.com>
2019-01-23 17:57:33 +0300

    lesson-07

--- app/src/main/java/com/roasloa/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/roasloa/coderswag/Controller/ProductsActivity.kt
@@ -11,6 +11,6 @@ class ProductsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        
+
     }
 }
