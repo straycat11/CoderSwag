90c1a6f3cd6a680976e6875c79a83475c8369b4e
utku <utkuuyucel@gmail.com>
2019-01-23 19:29:40 +0300

    lesson 08

--- app/src/main/java/com/roasloa/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/roasloa/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,35 @@
 package com.roasloa.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.roasloa.coderswag.Adapters.ProductAdapter
 import com.roasloa.coderswag.R
+import com.roasloa.coderswag.Services.DataService
 import com.roasloa.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductAdapter(this,DataService.getProducts(categoryType))
+
+        var spanCount = 2
+
+        val orientation = resources.configuration.orientation
+        when(orientation){
+            Configuration.ORIENTATION_LANDSCAPE -> spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
 
     }
 }
